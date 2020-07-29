d1aedd83b5f5e1aab2066bc2f1dc058b5dc7b3f0
FlaviaIoana <61621809+FlaviaIoana@users.noreply.github.com>
2020-07-29 11:00:19 +0300

    navbar-form-game

---
+++ build/precache-manifest.3d011f8f4a2f48c767b2d41ed7566764.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfad25541d55e7d2a6ea6f41d88bcf66",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d0c0569e8f4cbfa9733a",
+    "url": "/static/css/main.8175ac7b.chunk.css"
+  },
+  {
+    "revision": "9150bbe39c8f072b1d02",
+    "url": "/static/js/2.be55c6ec.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.be55c6ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0c0569e8f4cbfa9733a",
+    "url": "/static/js/main.1f068d7e.chunk.js"
+  },
+  {
+    "revision": "85f1076f9d8b815a5f95",
+    "url": "/static/js/runtime-main.b3eba484.js"
+  }
+]);
